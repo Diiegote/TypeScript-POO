ed53e00793c2131f056b4d1745b5e79c0aa7eacd
Diiegote <diego.elprado22@gmail.com>
2023-05-05 20:21:21 -0300

    get/update/findOne

--- src/app/03-main.ts
+++ src/app/03-main.ts
@@ -2,8 +2,33 @@ import { ProductHttpService } from "./service/product-http-service";
 
 
 (async () => {
-  const productService = new ProductHttpService();
+  try {
+    const productService = new ProductHttpService();
+    console.log('--'.repeat(10));
+    console.log('getAll');
+    const products = await productService.getAll();
+    console.log(products);
+    console.log(products.map(item => item.price));
 
-  const products = await productService.getAll();
-  console.log(products);
+
+    const productId = products[0].id;
+    console.log('--'.repeat(10));
+    console.log('Update');
+
+    const productEdit = await productService.update(productId, {
+      price: 25000,
+      title: 'New Titleee',
+      description: 'New Descriptionsss',
+    });
+    console.log('Producto editado', productEdit);
+
+    console.log('--'.repeat(10));
+    console.log('findOne');
+
+    const product = await productService.findOne(productId);
+    console.log(product);
+
+  } catch (error) {
+    console.log(error);
+  }
 })();
